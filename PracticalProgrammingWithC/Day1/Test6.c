dbb555b4a169559830d8607368ddf6f94e00a6d2
UttamPatnaik <uttampatnaik585@gmail.com>
2025-09-22 20:16:40 +0530

    Update Test6.c

--- PracticalProgrammingWithC/Day1/Test6.c
+++ PracticalProgrammingWithC/Day1/Test6.c
@@ -1,1 +1,16 @@
-
+//Write a program to check whether the input number is even/odd
+#include <stdio.h>
+int main(){
+    int inp;
+    printf("Enter a number: ");
+    scanf("%d",&inp);
+    if(inp % 2 ==0)
+    {
+        printf("It is an even number");
+    }
+    else
+    {
+        printf("It is an odd number");
+    }
+    return 0;
+}
