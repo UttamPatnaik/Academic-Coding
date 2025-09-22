3df7dcd80d75e83afb47266a30ab801f17cba791
UttamPatnaik <uttampatnaik585@gmail.com>
2025-09-22 20:29:08 +0530

    Add files via upload

---
+++ PracticalProgrammingWithC/Day2/q2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int inp;
+printf("Enter a no.");
+scanf("%d",&inp);
+if(inp%2==0)
+{
+printf("It is an even no.");
+}
+else
+{
+printf("It is an odd no.");
+}
+return 0;
+}
