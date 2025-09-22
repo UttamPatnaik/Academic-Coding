8ab853c3d8880ddd2117bc4ccd6f170e7b954a7a
UttamPatnaik <uttampatnaik585@gmail.com>
2025-09-22 20:16:59 +0530

    Update Test7.c

--- PracticalProgrammingWithC/Day1/Test7.c
+++ PracticalProgrammingWithC/Day1/Test7.c
@@ -1,1 +1,12 @@
-
+//to swap 2 numbers using 3 variables
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter 2 numbers: ");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    b=c-b;
+    a=c-a;
+    printf("Swapped numbers: 1st number: %d \t 2nd number: %d",a,b);
+    return 0;
+}
