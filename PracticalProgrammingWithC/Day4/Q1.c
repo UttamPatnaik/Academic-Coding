1c94ddc1dc769102ea5690d2e63182745eea4f79
UttamPatnaik <uttampatnaik585@gmail.com>
2025-09-23 15:53:47 +0530

    Update Q1.c

--- PracticalProgrammingWithC/Day4/Q1.c
+++ PracticalProgrammingWithC/Day4/Q1.c
@@ -1,1 +1,33 @@
-
+/* Write a program that calculates the user's body mass index using its height(inch) and weight(pounds) and categories it as underweight, normal, overweight, or obese.
+formula for BMI=(703*wt)/ht */
+#include <stdio.h>
+int main(){
+    double ht,wt,bmi;
+    printf("Enter your weight in pounds: ");
+    scanf("%lf",&wt);
+    printf("Enter your height in inches: ");
+    scanf("%lf",&ht);
+    bmi=(703*wt)/(ht*ht);
+    printf("Your BMI is:  %.2f \t",bmi);
+    if(bmi<18.5)
+    {
+        printf("Your BMI shows that you are underweight.");
+    }
+    else if(bmi>=18.5 && bmi<=24.9)
+    {
+        printf("Your BMI shows that you are normal weight.");
+    }
+    else if(bmi>=25 && bmi<=29.9)
+    {
+        printf("Your BMI shows that you are overweight.");
+    }
+    else if(bmi>=30)
+    {
+       printf("Your BMI shows that you are obese.");
+    }
+    else
+    {
+        printf("Invalid Input");
+    }
+    return 0;
+}
